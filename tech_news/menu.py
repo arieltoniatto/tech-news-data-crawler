f8c8b1a75b96232cfd7e3152b81c0e2b4684cfd5
arieltoniatto <ariel.toniatto@gmail.com>
2023-02-09 12:35:21 -0300

    feat: analyzer_menu input

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,12 @@
+menu_options = """Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por categoria;
+ 4 - Listar top 5 categorias;
+ 5 - Sair."""
+
+
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    input(menu_options)
